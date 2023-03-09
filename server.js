420976f63629ab8ad67bac1cb2ed61612c26e366
BartoszBuko <bukobartosz@gmail.com>
2023-03-09 16:04:33 +0000

    Added checking file size. Added 404 page.

--- server.js
+++ server.js
@@ -13,25 +13,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
-const storage = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "tmp/");
-    },
-    filename: function (req, file, cb) {
-      crypto.pseudoRandomBytes(16, function (err, raw) {
-        cb(
-          null,
-          raw.toString("hex") +
-            "OptiAmazed" +
-            "." +
-            mime.extension(file.mimetype)
-        );
-      });
-    },
-  }),
+const MAX_SIZE = 4 * 1024 * 1024;
+//  add error handling for file size
+const fileSizeLimitErrorHandler = (err, req, res, next) => {
+  if (err) {
+    res.sendStatus(413);
+  } else {
+    next();
+  }
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "tmp/");
+  },
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(
+        null,
+        raw.toString("hex") + "OptiAmazed" + "." + mime.extension(file.mimetype)
+      );
+    });
+  },
 });
-const upload = multer(storage);
+
+const upload = multer({ storage: storage, limits: { fileSize: MAX_SIZE } });
 
 const cheetaho = new CheetahO({
   api_key: process.env.CHEETAHO_API_KEY,
@@ -58,32 +64,41 @@ app.get("/", (req, res) => {
   res.render("index", { rendered: false, imageData: false, name: false });
 });
 
-app.post("/", upload.single("image"), async (req, res) => {
-  const file = req.file;
-  optSettings.file = file.path;
-  let data;
-  const customPromise = new Promise((resolve, reject) => {
-    cheetaho.optimizeUpload(optSettings, (err, res) => {
+app.post(
+  "/",
+  upload.single("image"),
+  fileSizeLimitErrorHandler,
+  async (req, res) => {
+    const file = req.file;
+    optSettings.file = file.path;
+    let data;
+    const customPromise = new Promise((resolve, reject) => {
+      cheetaho.optimizeUpload(optSettings, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+
+    data = await customPromise;
+    await fs.unlink(file.path, (err) => {
       if (err) {
-        reject(err);
-      } else {
-        resolve(res);
+        console.log(err);
       }
     });
-  });
 
-  data = await customPromise;
-  await fs.unlink(file.path, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+    res.render("index", {
+      rendered: true,
+      imageData: data.data.item,
+      name: file.originalname,
+    });
+  }
+);
 
-  res.render("index", {
-    rendered: true,
-    imageData: data.data.item,
-    name: file.originalname,
-  });
+app.all("*", (req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(port);
